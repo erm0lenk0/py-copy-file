b1e94ecb439ab739c0db274fe29cc5b37998b8cd
Ermolenko Andrey <f1nd6r@gmail.com>
2025-05-08 19:40:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    source_file = parts[1]
+    destination_file = parts[2]
+    if source_file == destination_file:
+        return
+
+    try:
+        with (open(source_file, "r") as file_in,
+              open(destination_file, "w") as file_out):
+            content = file_in.read()
+            file_out.write(content)
+    except FileNotFoundError:
+        return
